460dff9db3d2c0056862b2136a2ebd1049271e01
Yakushin Alexandr <valensiec08@mail.ru>
2021-10-23 21:46:11 +0300

    edit

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,15 +7,16 @@ class SQRServiceTest {
     @org.junit.jupiter.api.Test
     void serviceClass() {
         SQRService service = new SQRService();
-        int expected =3;
-        int actual = service.serviceClass(200,300);
-        assertEquals(expected,actual);
+        int expected = 3;
+        int actual = service.serviceClass(200, 300);
+        assertEquals(expected, actual);
     }
+
     @org.junit.jupiter.api.Test
     void serviceClass1() {
         SQRService service = new SQRService();
-        int expected =3;
-        int actual = service.serviceClass(300,400);
-        assertEquals(expected,actual);
+        int expected = 3;
+        int actual = service.serviceClass(300, 400);
+        assertEquals(expected, actual);
     }
 }
